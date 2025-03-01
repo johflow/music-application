e7badbf42a94003190e57b29ecccaa650b6f8672
Riya Patel <riyap@email.sc.edu>
2025-03-01 14:46:42 -0500

    Completed #120

---
+++ src/main/java/com/model/Chord.java
@@ -0,0 +1,29 @@
+package com.model;
+import java.util.ArrayList;
+
+public class Chord {
+    private ArrayList<Note> notes;
+
+    public Chord() {
+        //TODO
+    }
+
+    public void addNote(int pitch, double duration) {
+        //TODO
+    }
+
+    public Chord select() {
+        //TODO
+    }
+
+    public void delete() {
+        //TODO
+    }
+
+    public void play() {
+        //TODO
+    }
+
+
+
+}
