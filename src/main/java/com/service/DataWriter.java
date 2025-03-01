a9cd3c50523df901ce345d69917c68a23f714a2e
johflow <willflowersss@gmail.com>
2025-03-01 18:05:41 -0500

    Finished #112

---
+++ src/main/java/com/service/DataWriter.java
@@ -0,0 +1,15 @@
+package com.service;
+
+public class DataWriter {
+
+  public boolean saveSheetMusic() {
+    //TODO
+    return true;
+  }
+
+  public boolean saveUsers() {
+    //TODO
+    return true;
+  }
+
+}
