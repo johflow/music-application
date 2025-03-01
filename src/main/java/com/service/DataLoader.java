17d9931acb80bdbd12a34cf121759c9fb7e3efc3
johflow <willflowersss@gmail.com>
2025-03-01 18:06:31 -0500

    Finished #113

---
+++ src/main/java/com/service/DataLoader.java
@@ -0,0 +1,17 @@
+package com.service;
+
+import java.util.ArrayList;
+
+public class DataLoader {
+
+  public ArrayList<Song> getSongs() {
+    //TODO
+    return new ArrayList<Song>();
+  }
+
+  public ArrayList<User> getUsers() {
+    //TODO
+    return new ArrayList<User>();
+  }
+
+}
