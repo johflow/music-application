d24ca627e7f318375500d5282e203293675ff67a
Riya Patel <riyap@email.sc.edu>
2025-03-02 17:28:23 -0500

    Finished #115 and #116

---
+++ src/main/java/com/model/SheetMusic.java
@@ -0,0 +1,24 @@
+package com.model;
+
+import java.util.ArrayList;
+
+public class SheetMusic {
+    private String title;
+    private ArrayList<Measure> measures;
+    private Instrument instrument;
+    private int pickUp;
+
+    public SheetMusic(String titel, Instrument instrument, int pickUp) {
+        //TODO
+    }
+
+    public void transpose(String key) {
+        //TODO
+    }
+
+    public void display() {
+        //TODO
+    }
+
+
+}
