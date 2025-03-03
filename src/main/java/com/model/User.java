bd5a3d75b0c14332c589e17b7a75cd8188b2bb64
johflow <willflowersss@gmail.com>
2025-03-03 16:26:57 -0500

    Finished #130

--- src/main/java/com/model/User.java
+++ src/main/java/com/model/User.java
@@ -1,14 +1,42 @@
+package com.model;
+
+import com.model.Song;
+import java.util.ArrayList;
+import java.util.UUID;
+
 public class User {
+  
+  private UUID id;
+  private String email;
+  private String username;
+  private String password;
+  private ArrayList<Song> favoriteSongs;
+  private ArrayList<User> followedUsers;
+  private String themeColor;
 
-  public User() {
-    
+  public User(String email, String username, String password) {
+    this.email = email;
+    this.username = username;
+    this.password = password;
   }
 
-  public ArrayList<Song> getSongs() {
-    //TODO
+  public void addFavoriteSong(Song song) {
+    favoriteSongs.add(song);
   }
 
-  public ArrayList<Song> findSongs(String keyword) {
-    //TODO
+  public void removeFavoriteSong(Song song) {
+    favoriteSongs.remove(song);
+  }
+
+  public void followUser(User user) {
+    followedUsers.add(user);
+  }
+
+  public void unfollowUser(User user) {
+    followedUsers.remove(user);
+  }
+
+  public void changeTheme(String themeColor) {
+    this.themeColor = themeColor;
   }
 }
