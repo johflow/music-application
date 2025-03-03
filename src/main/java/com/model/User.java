8c211d34acfae16cae7a2c911a8aa09265dffea1
jaden-argaman <156003352+jaden-argaman@users.noreply.github.com>
2025-03-02 19:50:21 -0500

    Finished #110, #111 and #122

---
+++ src/main/java/com/model/User.java
@@ -0,0 +1,14 @@
+public class User {
+
+  public User() {
+    
+  }
+
+  public ArrayList<Song> getSongs() {
+    //TODO
+  }
+
+  public ArrayList<Song> findSongs(String keyword) {
+    //TODO
+  }
+}
