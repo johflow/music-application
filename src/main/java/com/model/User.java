1bb051878113612f63efca05817b81d2ccf70cb4
Riya Patel <riyap@email.sc.edu>
2025-03-04 09:46:23 -0500

    In progress for #126 and #127

--- src/main/java/com/model/User.java
+++ src/main/java/com/model/User.java
@@ -1,14 +1,75 @@
+package com.model;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
 public class User {
 
-  public User() {
-    
+  private UUID id;
+  private String email;
+  private String username;
+  private String password;
+  private ArrayList<Song> favoriteSongs;
+  private ArrayList<User> followedUsers;
+  private String themeColor;
+
+  public User(String email, String username, String password) {
+    this.id = UUID.randomUUID();
+    this.email = email;
+    this.username = username;
+    this.password = password;
+    this.favoriteSongs = new ArrayList<>();
+    this.followedUsers = new ArrayList<>();
+    this.themeColor = "default";
   }
 
-  public ArrayList<Song> getSongs() {
-    //TODO
+  public User(UUID id, String email, String username, String password) {
+    this.id = id;
+    this.email = email;
+    this.username = username;
+    this.password = password;
+    this.favoriteSongs = new ArrayList<>();
+    this.followedUsers = new ArrayList<>();
+    this.themeColor = "default";
   }
 
-  public ArrayList<Song> findSongs(String keyword) {
-    //TODO
-  }
+  public UUID getId() {
+    return id;
+}
+
+public String getEmail() {
+    return email;
+}
+
+public String getUsername() {
+    return username;
+}
+
+public String getPassword() {
+    return password;
+}
+
+public String getThemeColor() {
+    return themeColor;
+}
+
+public void addFavoriteSong(Song song) {
+    favoriteSongs.add(song);
+}
+
+public void removeFavoriteSong(Song song) {
+    favoriteSongs.remove(song);
+}
+
+public void followUser(User user) {
+    followedUsers.add(user);
+}
+
+public void unfollowUser(User user) {
+    followedUsers.remove(user);
+}
+
+public void changeTheme(String themeColor) {
+    this.themeColor = themeColor;
+}
 }
--- src/main/java/com/model/UserList.java
+++ src/main/java/com/model/UserList.java
@@ -1,21 +1,54 @@
+package com.model;
+
+import java.util.ArrayList;
+import com.service.DataLoader;
+import com.service.DataWriter;
+
 public class UserList {
-  
-  private UserList userList;
-  private ArrayList<User> users;
+    private static UserList userList;
+    private ArrayList<User> users;
 
-  private UserList() {
+    private UserList() {
+        users = DataLoader.getUser();
+        if (users == null) {
+            users = new ArrayList<>();
+        }
+    }
+
+    public static UserList getInstance() {
+        if (userList == null) {
+            userList = new UserList();
+        }
+        return userList;
+    }
+
+    public User getUser(String username) {
+        for (User user : users) {
+            if (user.getUsername().equals(username)) {
+                return user;
+            }
+        }
+        return null;
+    }
     
-  }
+    public void addUser(User user) {
+        if (user != null && getUser(user.getUsername()) == null) {
+            users.add(user);
+            save(); // Save after adding a user
+        }
+    }
+    
+    public void removeUser(User user) {
+        if (users.remove(user)) {
+            save(); // Save after removing a user
+        }
+    }
 
-  public UserList getInstance() {
-    //TODO
-  }
+    public void save() {
+        DataWriter.save(users);
+    }
 
-  public User getUser(String username) {
-    //TODO
-  }
-
-  public void save() {
-    //TODO
-  }
+    public ArrayList<User> getAllUsers() {
+        return new ArrayList<>(users);
+    }
 }
