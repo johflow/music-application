dbbc98e2e6a7e7a43f0b6c946cd29a97a25923e6
Riya Patel <riyap@email.sc.edu>
2025-03-06 14:18:43 -0500

    In progress #134 and #135

--- src/main/java/com/model/Song.java
+++ src/main/java/com/model/Song.java
@@ -7,21 +7,56 @@ public class Song {
     private UUID id;
     private String title;
     private String composer;
-    private ArrayList<SheetMusic> measures;
+    private ArrayList<SheetMusic> sheetMusics;
+    private int pickUp;
 
     public Song(String title, String composer) {
-        //TODO
+        this.id = UUID.randomUUID();
+        this.title = title;
+        this.composer = composer;
+        this.sheetMusics = new ArrayList<>();
+        this.pickUp = 0;
     }
 
-    public void display() {
-        //TODO
+    public Song(UUID id, String title, String composer) {
+        this.id =id);
+        this.title = title;
+        this.composer = composer;
+        this.sheetMusics = new ArrayList<>();
+        this.pickUp = 0;
+    }
+
+    public void addSheetMusic(Instrument instrument) {
+       
     }
 
     public void save() {
-        //TODO
+
     }
 
-    public void exportToPDF() {
-        //TODO
+    // Getters and setters
+    public UUID getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getComposer() {
+        return composer;
+    }
+
+    public ArrayList<SheetMusic> getSheetMusics() {
+        return sheetMusics;
+    }
+
+    public int getPickUp() {
+        return pickUp;
+    }
+
+    public void setPickUp(int pickUp) {
+        this.pickUp = pickUp;
     }
 }
+
