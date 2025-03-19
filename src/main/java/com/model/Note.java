a54c7a5a809fe94a99231dff9859de6409357ab6
johflow <willflowersss@gmail.com>
2025-03-19 16:03:28 -0400

    Finished Note class

--- src/main/java/com/model/Note.java
+++ src/main/java/com/model/Note.java
@@ -5,12 +5,15 @@ import org.jfugue.player.Player;
 /**
  * Represents a musical note
  */
-public class Note implements MusicElement {
+public class Note {
+	private static final String type = "note";
 	private double pitch;
-	private double duration;
-	private char durationChar;
 	private int midiNumber;
 	private String noteName;
+	private double duration;
+	private char durationChar;
+	private int dotted;
+	private boolean tied;
 	private String lyric;
 	private static final String[] NOTE_NAMES = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
 
@@ -19,15 +22,11 @@ public class Note implements MusicElement {
 	 * 
 	 * @param pitch    The pitch of the note (MIDI value)
 	 * @param duration The duration of the note
-	 * @param position The position of the note in the measure
 	 * @param isRest   Whether the note is a rest
 	 */
-	public Note(int pitch, int duration, int position, boolean isRest) {
+	public Note(int pitch, int duration, boolean isRest) {
 		this.pitch = pitch;
 		this.duration = duration;
-		this.position = position;
-		this.isRest = isRest;
-		this.isSelected = false;
 		findMissingAttributes();
 	}
 
@@ -82,9 +81,15 @@ public class Note implements MusicElement {
 		this.lyric = lyric;
 	}
 
-	public void play() {
-		Player player = new Player();
-		player.play(noteName + durationChar);
+  public Note(double pitch, int midiNumber, String noteName, double duration, char durationChar, int dotted, boolean tied, String lyric) {
+		this.pitch = pitch;
+		this.midiNumber = midiNumber;
+		this.noteName = noteName;
+		this.duration = duration;
+		this.durationChar = durationChar;
+		this.dotted = dotted;
+		this.tied = tied;
+		this.lyric = lyric;
 	}
 
 	private void findMissingAttributes() {
@@ -126,5 +131,96 @@ public class Note implements MusicElement {
 				duration = 0.0625;
 			}
 		}
+	}
+
+	public String toJFugueString(String tiesDurationChar) {
+		String JFugueString;
+		JFugueString = noteName + durationChar;
+		return JFugueString;
+	}
+
+	public char getDurationChar() {
+		return durationChar;
+	}
+	public int getDotted() {
+		return dotted;
+	}
+
+	public boolean hasTie() {
+		return tied;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public double getPitch() {
+		return pitch;
+	}
+
+	public int getMidiNumber() {
+		return midiNumber;
+	}
+
+	public String getNoteName() {
+		return noteName;
+	}
+
+	public double getDuration() {
+		return duration;
+	}
+
+	public String getLyric() {
+		return lyric;
+	}
+
+	public void setPitch(double pitch) {
+		if (pitch < 0) {
+			throw new IllegalArgumentException("Notes cannot have a pitch of zero or less!");
+		}
+		this.pitch = pitch;
+	}
+
+	public void setMidiNumber(int midiNumber) {
+		if (midiNumber < 0) {
+			throw new IllegalArgumentException("Notes cannot have a Midi Number of zero or less!");
+		}
+		this.midiNumber = midiNumber;
+	}
+
+	public void setNoteName(String noteName) {
+		if (noteName == null) {
+			throw new IllegalArgumentException("Note name cannot be null!");
+		}
+		this.noteName = noteName;
+	}
+
+	public void setDuration(double duration) {
+		if (duration <= 0 || duration > 2) {
+			throw new IllegalArgumentException("Invalid duration argument!");
+		}
+		this.duration = duration;
+	}
+
+	public void setDurationChar(char durationChar) {
+		this.durationChar = durationChar;
+	}
+
+	public void setDotted(int dotted) {
+		if (dotted < 0) {
+			throw new IllegalArgumentException("You cannot dot a note negative times!");
+		}
+		this.dotted = dotted;
+	}
+
+	public void setTied(boolean tied) {
+		this.tied = tied;
+	}
+
+	public void setLyric(String lyric) {
+		if (lyric == null) {
+			throw new IllegalArgumentException("Lyrics cannot be null! (Try making it \"\")");
+		}
+		this.lyric = lyric;
 	}
 }
