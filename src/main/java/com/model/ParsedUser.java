923c65ceb77a75ecc5f502ceba935260a02628dc
Riya Patel <riyap@email.sc.edu>
2025-03-25 14:09:52 -0400

    Fixed errors in #154 and #155

--- src/main/java/com/model/ParsedUser.java
+++ src/main/java/com/model/ParsedUser.java
@@ -3,7 +3,7 @@ package com.model;
 import java.util.List;
 import java.util.UUID;
 
-public record ParsedUser(User user, List<UUID> followedUsers, List<UUID> favoritedSongs) {
+public class ParsedUser {
 
   private final User user;
   private final List<UUID> followedUsers;
