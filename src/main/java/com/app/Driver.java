248a24ec411738a4b3c0ee3302c995c309bcbd57
Joshua Gould <joshuagould1604@gmail.com>
2025-03-26 12:08:58 -0400

    Finishing rough draft of scenario

--- src/main/java/com/app/Driver.java
+++ src/main/java/com/app/Driver.java
@@ -1,38 +1,77 @@
 package com.app;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import com.model.MusicAppFacade;
-import com.model.Song;
+import com.model.Instrument;
 import com.model.Measure;
-
+import com.model.MusicAppFacade;
+import com.model.MusicElement;
+import com.model.Note;
+import com.model.SheetMusic;
+import com.model.Song;
 
 public class Driver {
 
   public static void main(String[] args) {
     MusicAppFacade facade = MusicAppFacade.getInstance();
+    // Fred trying to register as Fellicia
     facade.register("ffredrickson", "mypassword", "fmail@gmail.com");
-
+    // Fred making his own unique account
     facade.register("ffred", "mypassword", "fmail@gmail.com");
     facade.logout();
 
+    // Fred logging in
     facade.login("ffred", "mypassword");
 
+    // Fred searching for Tom Petty songs
     List<Song> songs = facade.searchForSongs("Tom Petty");
     System.out.println(songs);
     
+    // Fred selecting Free Fallin', playing it, printing it, and logging out
     facade.setViewedSong(songs.get(2));
     facade.playViewedSong();
     facade.printViewedSong();
     facade.logout();
 
+    // Fellicia logging in
     facade.login("ffredrickson", "password123");
 
+    // Fellicia creating a new song
     facade.createSong("A horses journey", "Fellicia");
-    facade.addMeasure(new Measure(1, 1, 1, 1.0));
+
+    // Fellicia adds necessary prereqs for adding 2 measures with notes
+    facade.addSheetMusic(new SheetMusic(new Instrument(Arrays.asList("Treble"), "Voice")));
+
+    // Fellicia uses the UI to add notes to her 1st measure
+    List<MusicElement> notes1 = new ArrayList<>();
+    notes1.add(new Note("NOTE", 60.0, 60, "C4", 1.0, 'q', 0, false, ""));
+    notes1.add(new Note("NOTE", 62.0, 62, "D4", 1.0, 'q', 0, false, ""));
+    notes1.add(new Note("NOTE", 64.0, 64, "E4", 1.0, 'q', 0, false, ""));
+    notes1.add(new Note("NOTE", 65.0, 65, "F4", 1.0, 'q', 0, false, ""));
+    
+    facade.addMeasure(new Measure(0, 4, 4, 120, notes1));
+
+    // Fellicia uses the UI to add notes to her 2nd measure
+    List<MusicElement> notes2 = new ArrayList<>();
+    notes2.add(new Note("NOTE", 60.0, 60, "C4", 1.0, 'q', 0, false, ""));
+    notes2.add(new Note("NOTE", 62.0, 62, "D4", 1.0, 'q', 0, false, ""));
+    notes2.add(new Note("NOTE", 64.0, 64, "E4", 1.0, 'q', 0, false, ""));
+    notes2.add(new Note("NOTE", 65.0, 65, "F4", 1.0, 'q', 0, false, ""));
+    
+    facade.addMeasure(new Measure(0, 4, 4, 120, notes2));
+
+    // Fellicia plays the song and logs out
+    facade.playViewedSong();
+    facade.logout();
+    
+    // Fred logs in, searches for Fellicias new song and then plays it
+    facade.login("ffred", "mypassword");
+    facade.setViewedSong(facade.searchForSong("A horses journey"));
+    facade.playViewedSong();
+
+
 
   }
-
-
-
 }
