8bda3aac254ee03f0a1420727aa60f4785987ef3
jomago04 <joshuagould1604@gmail.com>
2025-04-15 14:53:59 -0400

    added link for FXML documentation

--- src/main/java/com/frontend/gui/HelloApplication.java
+++ src/main/java/com/frontend/gui/HelloApplication.java
@@ -8,7 +8,7 @@ import javafx.stage.Stage;
 
 
 import java.io.IOException;
-
+// https://jenkov.com/tutorials/javafx/fxml.html documentation for FXML
 public class HelloApplication extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
